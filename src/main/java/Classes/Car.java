9e76d6bf36b7faa1da17e5c1d03b26da00504a20
Rozelin OLĞAÇ <rolgac@innova.com.tr>
2024-06-04 14:15:03 +0300

    04.06

--- src/main/java/Classes/Car.java
+++ src/main/java/Classes/Car.java
@@ -1,4 +1,35 @@
 package Classes;
 
-public class Car {
+public class Car {   // Burada public yazılmasa da olur
+    String type="Sedan";
+    String model;
+    String color;
+    int speed;
+    int speedLimit = 180;
+
+    void increaseSpeed(int increment) {
+        if (speed < speedLimit) {
+            speed += increment;
+        }
+
+        if (speed > speedLimit) {
+            speed = 180;
+        }
+
+    }
+
+    void decreaseSpeed(int decrease) {
+        if (speed > 0) {
+            speed -= decrease;
+        }
+        if (speed < 0) {
+            speed = 0;
+        }
+    }
+
+    void printSpeed() {
+        System.out.println("Hızınız : " + speed);
+    }
+
+
 }
---
+++ src/main/java/Classes/Main.java
@@ -0,0 +1,32 @@
+package Classes;
+
+public class Main {
+    public static void main(String[] args) {
+        Car audi = new Car();
+        audi.model = "Audi A3";
+        System.out.println(audi.model);
+        System.out.println(audi.type);
+        audi.type = "Sports";
+        System.out.println(audi.type);
+        audi.speed = 10;
+        System.out.println(audi.model + " Hızı : " + audi.speed);
+        audi.increaseSpeed(190);
+        audi.printSpeed();
+
+
+        Car bmw = new Car();
+        bmw.model = "BMW";
+        bmw.speed = 20;
+        System.out.println(bmw.model + " Hızı : " + bmw.speed);
+        bmw.decreaseSpeed(1);
+        bmw.printSpeed();
+
+
+        Car mercedes = new Car();
+        mercedes.model = "Mercedes";
+        mercedes.speed = 30;
+        System.out.println(mercedes.model + " Hızı : " + mercedes.speed);
+
+    }
+}
+
